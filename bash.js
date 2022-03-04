5ecfab3b287d27af0d32e7daa951501410757a0f
Alec Eiber <aleceiber@gmail.com>
2022-03-04 12:34:38 -0500

    added pwd feature

---
+++ bash.js
@@ -0,0 +1,7 @@
+process.stdout.write("promp > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === "pwd") process.stdout.write(process.cwd());
+  process.stdout.write("\npromp > ");
+});
